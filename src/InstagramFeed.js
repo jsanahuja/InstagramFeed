018a8c108fb5be844bba77bdab1b9e44be5875c7
jsanahuja <bannss1@gmail.com>
2021-02-22 20:03:34 +0100

    Object.assign polyfill. Closes #41

--- src/InstagramFeed.js
+++ src/InstagramFeed.js
@@ -1,7 +1,7 @@
 /*
  * InstagramFeed
  *
- * @version 2.0.0
+ * @version 2.0.2
  *
  * https://github.com/jsanahuja/InstagramFeed
  *
@@ -59,6 +59,28 @@
         return str.replace(/[&<>"'`=\/]/g, function (char) {
             return escape_map[char];
         });
+    }
+
+    if (typeof Object.assign != 'function') {
+        Object.assign = function (target) {
+            'use strict';
+            if (target == null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            target = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source != null) {
+                    for (var key in source) {
+                        if (Object.prototype.hasOwnProperty.call(source, key)) {
+                            target[key] = source[key];
+                        }
+                    }
+                }
+            }
+            return target;
+        };
     }
 
     return function(opts) {
