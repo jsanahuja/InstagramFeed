a6c17d89ffb63adb4a1cf3afed08ca105d8b31be
evaleries <182410102083@cs.unej.ac.id>
2021-05-01 07:41:43 +0700

    feat: cdn url resolver
    uses custom cdn to load the images.

--- src/InstagramFeed.js
+++ src/InstagramFeed.js
@@ -29,6 +29,7 @@
         'display_igtv': false,
         'max_tries': 8,
         'callback': null,
+        'cdnUrlResolver': null,
         'styling': true,
         'items': 8,
         'items_per_row': 4,
@@ -228,6 +229,14 @@
         }
     }
 
+    function resolveUrl(url, callback) {
+        if (typeof callback === 'function') {
+            return callback(url);
+        }
+
+        return url;
+    }
+
     /**
      * Rendering
      */
@@ -283,7 +292,7 @@
          */
         if(options.display_profile && options.type !== "userid"){
             html += '<div class="instagram_profile"' + styles.profile_container + '>';
-            html += '<img class="instagram_profile_image" src="' + data.profile_pic_url  + '" alt="'+ (options.type == "tag" ? data.name + ' tag pic' : data.username + ' profile pic') + '"' + styles.profile_image + (options.lazy_load ? ' loading="lazy"' : '') + ' />';
+            html += '<img class="instagram_profile_image" src="' + resolveUrl(data.profile_pic_url, options.cdnUrlResolver)  + '" alt="'+ (options.type == "tag" ? data.name + ' tag pic' : data.username + ' profile pic') + '"' + styles.profile_image + (options.lazy_load ? ' loading="lazy"' : '') + ' />';
             if(options.type == "tag"){
                 html += '<p class="instagram_tag"' + styles.profile_name + '><a href="https://www.instagram.com/explore/tags/' + options.tag + '/" rel="noopener" target="_blank">#' + options.tag + '</a></p>';
             }else if(options.type == "username"){
@@ -334,7 +343,7 @@
                     }
 
                     html += '<a href="' + url + '"' + (options.display_captions ? ' data-caption="' + caption + '"' : '') + ' class="instagram-' + type_resource + '" rel="noopener" target="_blank"' + styles.gallery_image_link + '>';
-                    html += '<img' + (options.lazy_load ? ' loading="lazy"' : '') + ' src="' + image + '" alt="' + caption + '"' + styles.gallery_image + ' />';
+                    html += '<img' + (options.lazy_load ? ' loading="lazy"' : '') + ' src="' + resolveUrl(image, options.cdnUrlResolver) + '" alt="' + caption + '"' + styles.gallery_image + ' />';
                     html += '</a>';
                 }
                 html += '</div>';
@@ -360,7 +369,7 @@
                     caption = escape_string(caption);
 
                     html += '<a href="' + url + '"' + (options.display_captions ? ' data-caption="' + caption + '"' : '') + ' rel="noopener" target="_blank"' + styles.gallery_image_link + '>';
-                    html += '<img' + (options.lazy_load ? ' loading="lazy"' : '') + ' src="' + igtv[i].node.thumbnail_src + '" alt="' + caption + '"' + styles.gallery_image + ' />';
+                    html += '<img' + (options.lazy_load ? ' loading="lazy"' : '') + ' src="' + resolveUrl(igtv[i].node.thumbnail_src) + '" alt="' + caption + '"' + styles.gallery_image + ' />';
                     html += '</a>';
                 }
                 html += '</div>';
@@ -416,6 +425,11 @@
 
         if (options.callback == null && options.container == "") {
             options.on_error("Instagram Feed: Error, neither container found nor callback defined.", 2);
+            return false;
+        }
+
+        if (options.cdnUrlResolver != null && typeof options.cdnUrlResolver !== 'function') {
+            options.on_error("Instagram Feed: options.cdnUrlResolver must be a function or null.");
             return false;
         }
 
